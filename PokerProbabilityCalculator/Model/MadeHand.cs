a2da6b939ce4baeeab5c8cd5422d63b118f4e9cc
jccaldw1 <jacobccaldwell@outlook.com>
2023-03-12 21:32:20 -0700

    Added a constant in the MadeHand class for the worst possible hand.

--- PokerProbabilityCalculator/Model/MadeHand.cs
+++ PokerProbabilityCalculator/Model/MadeHand.cs
@@ -1,3 +1,9 @@
 ﻿namespace PokerProbabilityCalculator.Model;
 
-public record class MadeHand(Hand Hand, Value Value, Value? Value2, Value? Value3, Value? Value4, Value? Value5);
+public record class MadeHand(Hand Hand, Value Value, Value? Value2, Value? Value3, Value? Value4, Value? Value5)
+{
+    /// <summary>
+    /// The lowest possible made hand, 7-high.
+    /// </summary>
+    public static MadeHand LowestPossibleMadeHand { get; } = new MadeHand(Hand.HighCard, Value.Num7, Value.Num5, Value.Num4, Value.Num3, Value.Num2);
+};
