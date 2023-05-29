1e4428c810db6f58fd6c77d3f90be09d0afaf2bc
jccaldw1 <jacobccaldwell@outlook.com>
2023-05-28 17:32:16 -0700

    straight ossibility service now considers the deck

--- PokerProbabilityCalculator/Service/StraightPossibilityService.cs
+++ PokerProbabilityCalculator/Service/StraightPossibilityService.cs
@@ -2,39 +2,45 @@
 
 namespace PokerProbabilityCalculator.Service;
 
+/// <summary>
+/// Given a player hand and a board, determines whether a straight flush or straight can be made.
+/// </summary>
+/// <remarks>
+/// This service does not take into account the whole deck - it only determines whether there is a straight possible given the cards in play and the number of cards left to be played.
+/// </remarks>
 public static class StraightPossibilityService
 {
     private static readonly Value[] orderedStraightyValues = {
         Value.A, Value.Num2, Value.Num3, Value.Num4, Value.Num5, Value.Num6, Value.Num7, Value.Num8, Value.Num9, Value.Num10, Value.J, Value.Q, Value.K, Value.A
     };
 
-    public static bool CanMakeStraight(PlayerHand hand, Board board)
+    public static bool CanMakeStraight(PlayerHand hand, Board board, Deck deck)
     {
         int numberOfCardsPlayed = board.NumberOfCardsPlayed();
 
         List<Card> cardsInPlay = new()
         {
-            hand.Card2,
+            hand.Card1,
             hand.Card2
         };
 
         board.GetCards().ForEach(cardsInPlay.Add);
 
-        for(int i = 0; i < orderedStraightyValues.Length; i++)
+        for(int i = 0; i < orderedStraightyValues.Length - 4; i++)
             if (cardsInPlay.Any(card => card.Value == orderedStraightyValues[i]))
-                if(CheckIfThereIsAStraightPossibleStartingFromThisCard(i, cardsInPlay, numberOfCardsPlayed))
+                if(CheckIfThereIsAStraightPossibleStartingFromThisCard(i, cardsInPlay, numberOfCardsPlayed, deck))
                     return true;
 
         return false;
     }
 
-    public static bool CanMakeStraightFlush(PlayerHand hand, Board board, Suit suit)
+    public static bool CanMakeStraightFlush(PlayerHand hand, Board board, Suit suit, Deck deck)
     {
         int numberOfCardsPlayed = board.NumberOfCardsPlayed();
 
         List<Card> cardsInPlay = new()
         {
-            hand.Card2,
+            hand.Card1,
             hand.Card2
         };
 
@@ -42,14 +48,14 @@ public static class StraightPossibilityService
 
         cardsInPlay = cardsInPlay.Where(card => card.Suit == suit).ToList();
 
-        for(int i = 0; i < orderedStraightyValues.Length - 5; i++)
-            if(CheckIfThereIsAStraightPossibleStartingFromThisCard(i, cardsInPlay, numberOfCardsPlayed))
+        for(int i = 0; i < orderedStraightyValues.Length - 4; i++)
+            if(CheckIfThereIsAStraightPossibleStartingFromThisCard(i, cardsInPlay, numberOfCardsPlayed, deck))
                 return true;
 
         return false;
     }
 
-    private static bool CheckIfThereIsAStraightPossibleStartingFromThisCard(int indexOfValue, List<Card> cardsInPlay, int numberOfCardsThatNeedToContributeToStraight)
+    private static bool CheckIfThereIsAStraightPossibleStartingFromThisCard(int indexOfValue, List<Card> cardsInPlay, int numberOfCardsThatNeedToContributeToStraight, Deck deck)
     {
         // We need a certain number of cards within the straight range.
         List<Value> possibleStraightyValues = new()
@@ -63,7 +69,15 @@ public static class StraightPossibilityService
 
         if(cardsInPlay.Where(card => possibleStraightyValues.Contains(card.Value)).Count() >= numberOfCardsThatNeedToContributeToStraight)
         {
-            return true;
+            bool thisStraightExistsInDeck = true;
+
+            foreach(Value value in possibleStraightyValues)
+
+                if(!deck.cards.Any(card => card.Value == value))
+                    thisStraightExistsInDeck = false;
+
+            if(thisStraightExistsInDeck)
+                return true;
         }
 
         return false;
