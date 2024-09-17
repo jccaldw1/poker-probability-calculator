107dec073d2463762be08b7df699816e87e13987
jccaldw1 <jacobccaldwell@outlook.com>
2024-09-16 17:56:58 -0700

    instead of determining whether made hands are possible, find the actual cards that could be used to make them.

--- PokerProbabilityCalculator/Service/MadeHandPossibilityService.cs
+++ PokerProbabilityCalculator/Service/MadeHandPossibilityService.cs
@@ -3,7 +3,7 @@
 namespace PokerProbabilityCalculator.Service;
 
 /// <summary>
-/// Given a list of hands and a board, determines whether each hand can be made. 
+/// Given a list of hands and a board, determines whether each hand can be made. If a hand cannot be made, returns null. If it can be made, returns the list of cards in the deck that can be added to the board to make that hand.
 /// </summary>
 public class MadeHandPossibilityService
 {
@@ -26,7 +26,11 @@ public class MadeHandPossibilityService
         });
     }
 
-    public bool isRoyalFlushPossible(PlayerHand hand)
+    /// <returns>
+    /// The cards that need to be dealt in order to make a royal flush with the passed hand. If a royal flush
+    /// is not possible, returns null.
+    /// </returns>
+    public List<Card>? NecessaryRoyalFlushCards(PlayerHand hand)
     {
         List<Value> BroadwayCards = new()
         {
@@ -60,6 +64,8 @@ public class MadeHandPossibilityService
                 suitsThatCouldHaveRoyalFlush.Add(suit);
         }
 
+        List<Card>? cardsThatCouldMakeRoyalFlush = null;
+
         // Additionally, we need to examine the deck to determine if the cards needed are there.
         foreach (Suit suit in suitsThatCouldHaveRoyalFlush)
         {
@@ -72,15 +78,17 @@ public class MadeHandPossibilityService
 
             var missingRoyalFlushCards = royalFlushCardsForThisSuit.Except(suitedBroadwayCards).ToList();
 
-            // Just break if we can find a royal flush.
             if (_deck.Contains(missingRoyalFlushCards))
-                return true;
+            {
+                cardsThatCouldMakeRoyalFlush ??= new();
+                cardsThatCouldMakeRoyalFlush.AddRange(missingRoyalFlushCards);
+            }
         }
 
-        return false;
+        return cardsThatCouldMakeRoyalFlush;
     }
 
-    public bool isStraightFlushPossible(PlayerHand hand)
+    public List<Card>? NecessaryStraightFlushCards(PlayerHand hand)
     {
         List<Value> straightyValues = new()
         {
@@ -106,17 +114,29 @@ public class MadeHandPossibilityService
                 possibleStraightFlushSuits.Add(suit);
         }
 
+        List<Card>? cardsThatCouldMakeStraightFlush = null;
+
         foreach(Suit suit in possibleStraightFlushSuits)
         {
-            if(StraightPossibilityService.CanMakeStraightFlush(hand, _board, suit, _deck))
-                return true;
+            List<Card>? straightFlushCards = StraightPossibilityService.CanMakeStraightFlush(hand, _board, suit, _deck);
+
+            if(straightFlushCards != null)
+            {
+                straightFlushCards.ForEach(card =>
+                {
+                    if(cardsInPlay.Select(cardInPlay => cardInPlay.Value).Contains(card.Value))
+                        straightFlushCards.Remove(card);
+                });
+
+                cardsThatCouldMakeStraightFlush ??= new();
+                cardsThatCouldMakeStraightFlush.AddRange(straightFlushCards);
+            }
         }
 
-        return false;
-
+        return cardsThatCouldMakeStraightFlush;
     }
 
-    public bool isFourOfAKindPossible(PlayerHand hand)
+    public List<Card>? NecessaryCardsToMakeFourOfAKind(PlayerHand hand)
     {
         // Four of a kind requires only four cards.
         int numberOfCardsThatNeedToContributeToFourOfAKind = Math.Max(4 - _board.GetCards().Count, 0);
@@ -126,6 +146,7 @@ public class MadeHandPossibilityService
             hand.Card1,
             hand.Card2
         };
+
         _board.GetCards().ForEach(cardsInPlay.Add);
 
         List<KeyValuePair<Value, int>> frequenciesOfValues = new();
@@ -145,16 +166,23 @@ public class MadeHandPossibilityService
                 valuesToLookFor.Add(frequency.Key);
         });
 
+        List<Card>? cardsThatMakeFourOfAKind = null;
+
         foreach(Value value in valuesToLookFor)
         {
-            if (_deck.cards.Where(card => card.Value == value).Count() + frequenciesOfValues.First(card => card.Key == value).Value == 4)
-                return true;
+            IEnumerable<Card> cardsInDeckWithValue = _deck.cards.Where(card => card.Value == value);
+
+            if (cardsInDeckWithValue.Count() + frequenciesOfValues.First(card => card.Key == value).Value == 4)
+            {
+                cardsThatMakeFourOfAKind ??= new();
+                cardsThatMakeFourOfAKind.AddRange(cardsInDeckWithValue);
+            }
         }
 
-        return false;
+        return cardsThatMakeFourOfAKind;
     }
 
-    public bool isFullHousePossible(PlayerHand hand)
+    public List<Card>? NecessaryCardsToMakeFullHouse(PlayerHand hand)
     {
         // We need to check that both a three-of-a-kind and a pair of a different value are possible.
         List<Value> possibleThreeOfAKindValues = new();
@@ -191,20 +219,30 @@ public class MadeHandPossibilityService
                 possiblePairValues.Add(valueFrequency.Key);
         }
 
+        List<Card>? cardsThatCouldHelpMakeFullHouse = null;
+
         foreach(var possibleThreeOfAKindValue in possibleThreeOfAKindValues)
         {
-            if (possiblePairValues.Where(possiblePair => possiblePair != possibleThreeOfAKindValue).Count() > 0)
-                return true;
+            IEnumerable<Value> possiblePairValuesExcludingThreeOfAKindValue = possiblePairValues.Where(possiblePair => possiblePair != possibleThreeOfAKindValue);
+
+            if (possiblePairValuesExcludingThreeOfAKindValue.Count() > 0)
+            {
+                cardsThatCouldHelpMakeFullHouse ??= new();
+                cardsThatCouldHelpMakeFullHouse.AddRange(_deck.cards.Where(card => card.Value == possibleThreeOfAKindValue));
+                cardsThatCouldHelpMakeFullHouse.AddRange(_deck.cards.Where(card => possiblePairValuesExcludingThreeOfAKindValue.Contains(card.Value)));
+            }
         }
 
-        return false;
+        return cardsThatCouldHelpMakeFullHouse;
     }
 
-    public bool isFlushPossible(PlayerHand hand)
+    public List<Card>? NecessaryCardsToMakeFlush(PlayerHand hand)
     {
         List<Suit> possibleFlushSuits = new();
 
         int cardsThatNeedToContributeToFlush = _board.NumberOfCardsPlayed();
+
+        List<Card>? cardsThatCouldMakeFlush = null;
 
         foreach(Suit suit in Enum.GetValues(typeof(Suit)))
         {
@@ -219,26 +257,31 @@ public class MadeHandPossibilityService
             cardsInPlayOfThisSuit.AddRange(_board.GetCards().Where(card => card.Suit == suit));
 
             if(cardsInPlayOfThisSuit.Count() >= cardsThatNeedToContributeToFlush)
-                return true;
+            {
+                var cardsInDeckOfThisSuit = _deck.cards.Where(card => card.Suit == suit);
+
+                // Check that the deck has enough cards to make a flush.
+                if(cardsInDeckOfThisSuit.Count() >= 5 - cardsInPlayOfThisSuit.Count())
+                {
+                    cardsThatCouldMakeFlush ??= new();
+                    cardsThatCouldMakeFlush.AddRange(cardsInDeckOfThisSuit);
+                }
+            }
         }
 
-        return false;
+        return cardsThatCouldMakeFlush;
     }
 
-    // TODO: fix this. Either make the straightpossibilityservice take into account the deck (probably this) or make it return some indication of which straights are possible.
-    public bool isStraightPossible(PlayerHand hand)
-    {
-        // Once we know we can make a straight with the given hand, 
-        return StraightPossibilityService.CanMakeStraight(hand, _board, _deck);
-    }
+    public List<Card>? NecessaryCardsToMakeStraight(PlayerHand hand) => StraightPossibilityService.CanMakeStraight(hand, _board, _deck);
 
-    public bool isThreeOfAKindPossible(PlayerHand hand)
+    public List<Card>? isThreeOfAKindPossible(PlayerHand hand)
     {
         List<Card> cardsInPlay = new()
         {
             hand.Card1,
             hand.Card2
         };
+
         _board.GetCards().ForEach(cardsInPlay.Add);
 
         int cardsLeftToBePlayed = 5 - _board.NumberOfCardsPlayed();
@@ -267,26 +310,33 @@ public class MadeHandPossibilityService
             }
         });
 
+        List<Card>? cardsThatCouldMakeThreeOfAKind = null;
+
         foreach(var frequency in valueFrequencies)
         {
             int cardsNeededForThereToBeThreeOfThisValue = Math.Max(3 - cardsLeftToBePlayed, 0);
 
+            // TODO: Figure out what to do in this case: the hand being looked for is already on the board.
             // There already exists a three-of-a-kind dummy!!!!
-            if (frequency.Value >= 3)
-                return true;
+            //if (frequency.Value >= 3)
+            //    return true;
 
             // If this card is frequent enough to potentially have 3 by the river:
             if(frequency.Value >= cardsNeededForThereToBeThreeOfThisValue)
-
+            {
                 // Then it is possible to make three of a kind, as long as the deck contains enough of that card.
                 if(_deck.cards.Where(card => card.Value == frequency.Key).Count() >= 3 - frequency.Value)
-                    return true;
+                {
+                    cardsThatCouldMakeThreeOfAKind ??= new();
+                    cardsThatCouldMakeThreeOfAKind.AddRange(_deck.cards.Where(card => card.Value == frequency.Key));
+                }
+            }
         }
 
-        return false;
+        return cardsThatCouldMakeThreeOfAKind;
     }
 
-    public bool isTwoPairPossible(PlayerHand hand)
+    public List<Card>? isTwoPairPossible(PlayerHand hand)
     {
         List<Card> cardsInPlay = new()
         {
@@ -303,36 +353,40 @@ public class MadeHandPossibilityService
             if (cardsInPlay.Where(cardToCheck => cardToCheck.Value == card.Value).Count() >= 2)
                 valuesWithPairs.Add(card.Value);
 
-        if (valuesWithPairs.Count() >= 2)
-            return true;
+        //if (valuesWithPairs.Count() >= 2)
+        //    return true;
+
+        List<Card>? cardsThatCouldHelpMakeTwoPair = null;
 
         // If we have no pairs and there are four or more cards played, we cannot possibly make two pair.
         if (valuesWithPairs.Count() == 0 && _board.NumberOfCardsPlayed() >= 4)
         {
-            return false;
+            return cardsThatCouldHelpMakeTwoPair;
         }
 
         List<Value> valuesInPlayWithoutPairs = cardsInPlay.Select(card => card.Value).Distinct().Except(valuesWithPairs).ToList();
 
+        int numberOfPairs = valuesWithPairs.Count();
+
         // We now have at least two cards to work with.
-        for (int i = 0; i < 2 - valuesWithPairs.Count(); i++)
+        for (int i = 0; i < 2 - numberOfPairs; i++)
         {
             valuesInPlayWithoutPairs.ForEach(value =>
             {
                 // If we can find a card to pair this value, add it.
                 if (_deck.cards.Any(card => card.Value == value))
                 {
-                    valuesInPlayWithoutPairs.Remove(value);
-                    valuesWithPairs.Add(value);
+                    cardsThatCouldHelpMakeTwoPair ??= new();
+                    cardsThatCouldHelpMakeTwoPair.AddRange(_deck.cards.Where(card => card.Value == value));
                 }
             });
         }
 
         // Now we have found some more potential pairs. So we can simply check if valuesWithPairs has at least two pairs.
-        if (valuesWithPairs.Count() >= 2)
-            return true;
+        if (valuesWithPairs.Count() < 2)
+            return null;
 
-        return false;
+        return cardsThatCouldHelpMakeTwoPair;
     }
 
     public bool isPairPossible(PlayerHand hand)
@@ -348,22 +402,37 @@ public class MadeHandPossibilityService
         List<Value> valuesInPlay = new();
 
         foreach(Card card in cardsInPlay)
+        {
             // If the value already is in the list of values, then we have a pair.
             if (valuesInPlay.Contains(card.Value))
+            {
                 return true;
+            }
             else
+            {
                 valuesInPlay.Add(card.Value);
+            }
+
+        }
 
         // At this point all the values in play are unique. If we found a duplicate we have already returned true.
 
         if(_board.NumberOfCardsPlayed() < 5)
+        {
             foreach(Value value in valuesInPlay)
+            {
                 // If we can find another card with this value, return true;
                 if (_deck.cards.Any(card => card.Value == value))
+                {
                     return false;
+                }
+            }
+        }
         else
+        {
             // If we have played all cards and don't have a pair, a pair isn't possible. Amazing!!!!
             return false;
+        }
 
         return false;
     }
