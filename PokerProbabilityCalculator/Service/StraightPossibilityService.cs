d135dc33aece434e2c317b719fcecba2297c90e7
jccaldw1 <jacobccaldwell@outlook.com>
2024-09-16 17:58:59 -0700

    find cards that can make possible straights

--- PokerProbabilityCalculator/Service/StraightPossibilityService.cs
+++ PokerProbabilityCalculator/Service/StraightPossibilityService.cs
@@ -14,7 +14,7 @@ public static class StraightPossibilityService
         Value.A, Value.Num2, Value.Num3, Value.Num4, Value.Num5, Value.Num6, Value.Num7, Value.Num8, Value.Num9, Value.Num10, Value.J, Value.Q, Value.K, Value.A
     };
 
-    public static bool CanMakeStraight(PlayerHand hand, Board board, Deck deck)
+    public static List<Card>? CanMakeStraight(PlayerHand hand, Board board, Deck deck)
     {
         int numberOfCardsPlayed = board.NumberOfCardsPlayed();
 
@@ -26,18 +26,11 @@ public static class StraightPossibilityService
 
         board.GetCards().ForEach(cardsInPlay.Add);
 
-        for(int i = 0; i < orderedStraightyValues.Length - 4; i++)
-            if (cardsInPlay.Any(card => card.Value == orderedStraightyValues[i]))
-                if(CheckIfThereIsAStraightPossibleStartingFromThisCard(i, cardsInPlay, numberOfCardsPlayed, deck))
-                    return true;
-
-        return false;
+        return FindPossibleStraights(cardsInPlay, numberOfCardsPlayed, deck);
     }
 
-    public static bool CanMakeStraightFlush(PlayerHand hand, Board board, Suit suit, Deck deck)
+    public static List<Card>? CanMakeStraightFlush(PlayerHand hand, Board board, Suit suit, Deck deck)
     {
-        int numberOfCardsPlayed = board.NumberOfCardsPlayed();
-
         List<Card> cardsInPlay = new()
         {
             hand.Card1,
@@ -46,40 +39,115 @@ public static class StraightPossibilityService
 
         board.GetCards().ForEach(cardsInPlay.Add);
 
-        cardsInPlay = cardsInPlay.Where(card => card.Suit == suit).ToList();
+        List<Card> cardsOfSameSuitInPlay = cardsInPlay.Where(card => card.Suit == suit).ToList();
 
-        for(int i = 0; i < orderedStraightyValues.Length - 4; i++)
-            if(CheckIfThereIsAStraightPossibleStartingFromThisCard(i, cardsInPlay, numberOfCardsPlayed, deck))
-                return true;
+        List<Card> cardsThatCanMakeStraightFlush = new();
 
-        return false;
-    }
-
-    private static bool CheckIfThereIsAStraightPossibleStartingFromThisCard(int indexOfValue, List<Card> cardsInPlay, int numberOfCardsThatNeedToContributeToStraight, Deck deck)
-    {
-        // We need a certain number of cards within the straight range.
-        List<Value> possibleStraightyValues = new()
+        // In short, if a flush is possible, look for potential straight flushes.
+        if (cardsOfSameSuitInPlay.Count >= board.NumberOfCardsPlayed())
         {
-            orderedStraightyValues[indexOfValue],
-            orderedStraightyValues[indexOfValue + 1],
-            orderedStraightyValues[indexOfValue + 2],
-            orderedStraightyValues[indexOfValue + 3],
-            orderedStraightyValues[indexOfValue + 4]
-        };
+            for (int i = 0; i < orderedStraightyValues.Length - 4; i++)
+            {
+                // We need a certain number of cards within the straight range.
+                List<Value> possibleStraightyValues = new()
+                {
+                    orderedStraightyValues[i],
+                    orderedStraightyValues[i + 1],
+                    orderedStraightyValues[i + 2],
+                    orderedStraightyValues[i + 3],
+                    orderedStraightyValues[i + 4]
+                };
 
-        if(cardsInPlay.Where(card => possibleStraightyValues.Contains(card.Value)).Count() >= numberOfCardsThatNeedToContributeToStraight)
+                bool thisStraightIsPossible = true;
+                
+                foreach (Card card in cardsOfSameSuitInPlay)
+                {
+                    if (!possibleStraightyValues.Contains(card.Value))
+                    {
+                        thisStraightIsPossible = false;
+                        break;
+                    }
+                }
+
+                if (!thisStraightIsPossible) continue;
+                
+                // Find what values are not in play and look for them in the deck.
+                foreach (Card card in cardsOfSameSuitInPlay)
+                {
+                    possibleStraightyValues.Remove(card.Value);
+                }
+
+                List<Card> cardsToLookFor = new();
+                
+                foreach (Value value in possibleStraightyValues)
+                {
+                    cardsToLookFor.Add(new Card(suit, value));
+                }
+
+                bool cardsToLookForAreInDeck = true;
+
+                foreach (Card card in cardsToLookFor)
+                {
+                    if(!deck.cards.Contains(card))
+                        cardsToLookForAreInDeck = false;
+                }
+
+                if (cardsToLookForAreInDeck)
+                {
+                    cardsThatCanMakeStraightFlush.AddRange(cardsToLookFor);
+                }
+            }
+        }
+        else
         {
-            bool thisStraightExistsInDeck = true;
-
-            foreach(Value value in possibleStraightyValues)
-
-                if(!deck.cards.Any(card => card.Value == value))
-                    thisStraightExistsInDeck = false;
-
-            if(thisStraightExistsInDeck)
-                return true;
+            return null;
         }
 
-        return false;
+        if (cardsThatCanMakeStraightFlush.Count > 0) return cardsThatCanMakeStraightFlush;
+
+        return null;
+    }
+
+    private static List<Card>? FindPossibleStraights(List<Card> cardsInPlay, int numberOfCardsThatNeedToContributeToStraight, Deck deck)
+    {
+        List<Card>? cardsThatCouldMakeStraight = null;
+
+        for(int i = 0; i < orderedStraightyValues.Length - 4; i++)
+        {
+            // We need a certain number of cards within the straight range.
+            List<Value> possibleStraightyValues = new()
+            {
+                orderedStraightyValues[i],
+                orderedStraightyValues[i + 1],
+                orderedStraightyValues[i + 2],
+                orderedStraightyValues[i + 3],
+                orderedStraightyValues[i + 4]
+            };
+
+            if(cardsInPlay.Count(card => possibleStraightyValues.Contains(card.Value)) >= numberOfCardsThatNeedToContributeToStraight)
+            {
+                bool thisStraightExistsInDeck = true;
+
+                foreach(Value value in possibleStraightyValues)
+                    if(!deck.cards.Any(card => card.Value == value))
+                        thisStraightExistsInDeck = false;
+
+                if (thisStraightExistsInDeck)
+                {
+                    cardsThatCouldMakeStraight ??= new();
+
+                    // Just pick a card in the deck that has the value. We know the deck contains these values.
+                    foreach(Value value in possibleStraightyValues)
+                    {
+                        if(cardsThatCouldMakeStraight.Any(card => card.Value == value))
+                            continue;
+
+                        cardsThatCouldMakeStraight.Add(deck.cards.First(card => card.Value == value));
+                    }
+                }
+            }
+        }
+
+        return cardsThatCouldMakeStraight;
     }
 }
